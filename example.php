a79367fe966a5e689e306a86ce5afbf01c207839
Remdev <Remdev@users.noreply.github.com>
2014-01-27 07:59:00 -0800

    Add example

--- example.php
+++ example.php
@@ -7,7 +7,7 @@
  */
 
 require_once('CcexAPI.php');
-$CcexAPI = new CcexAPI();
+$CcexAPI = new CcexAPI({YOUR_API_KEY});
 
 $pairs = $CcexAPI->getPairs();
 print_r($pairs);
@@ -15,3 +15,6 @@ print_r($pairs);
 
 $tickers = $CcexAPI->getTickerInfo('btc-usd');
 print_r($tickers);
+
+$orders = $CcexAPI->getOrders('btc-usd');
+print_r($orders);
