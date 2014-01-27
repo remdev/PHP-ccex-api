019807a1951c811d6f48181be73b7c9d745664f3
Remdev <Remdev@users.noreply.github.com>
2014-01-27 07:57:55 -0800

    Add getting orders

--- CcexAPI.php
+++ CcexAPI.php
@@ -8,9 +8,15 @@
 
 class CcexAPI {
     
-    protected $api_url = "https://c-cex.com/t/";
+    protected $api_url = 'https://c-cex.com/t/';
+    protected $api_key;
     
-    protected function query($url) {
+    
+    public function __construct($api_key = '') {
+        $this->api_key = $api_key;
+    }
+    
+    protected function jsonQuery($url) {
         $opts = array('http' =>
             array(
                 'method'  => 'GET',
@@ -20,18 +26,25 @@ class CcexAPI {
         
         $context  = stream_context_create($opts);
         $feed = file_get_contents($url, false, $context);
-        return $feed;
+        return json_decode($feed,true);
     }
     
     
     public function getTickerInfo($pair){
-        return json_decode($this->query($this->api_url.$pair.'.json'), true );
+        $json = $this->jsonQuery($this->api_url.$pair.'.json');
+        return $json['ticker'];
     }
     
     
     public function getPairs(){
-       $json = json_decode( $this->query($this->api_url.'pairs.json'), true ); 
+       $json = $this->jsonQuery($this->api_url.'pairs.json'); 
        return $json['pairs'];
+    }
+    
+    
+    public function getOrders($pair){
+        $json =  $this->jsonQuery($this->api_url."r.html?key={$this->api_key}&a=orderlist&pair={$pair}");
+        return $json['return'];
     }
 }
 
