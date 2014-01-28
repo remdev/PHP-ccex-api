03ca5f7104d88d395fc03d201a1fe0e665918402
Remdev <Remdev@users.noreply.github.com>
2014-01-28 05:20:07 -0800

    Update example.php

--- example.php
+++ example.php
@@ -16,5 +16,14 @@ print_r($pairs);
 $tickers = $CcexAPI->getTickerInfo('btc-usd');
 print_r($tickers);
 
-$orders = $CcexAPI->getOrders('btc-usd');
+$orders = $CcexAPI->getOrders('btc-usd',1);//1 for self orders and 0 for all orders, default = 0
 print_r($orders);
+
+$history = $CcexAPI->getHistory('btc-usd',1389398400,time());//start time and end time in Unix Timestamp.
+print_r($history);
+
+$status = $CcexAPI->makeOrder('sell','btc-usd',100,820);//type,pair,quantity and price
+print_r($status);
+
+$cancel = $CcexAPI->cancelOrder(1228);// Order id
+print_r($cancel);
